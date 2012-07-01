a0ea13102fdaba21c233e092bab63f7185abe399
Jerry Sievert <code@legitimatesounding.com>
2012-07-01 14:55:52 -0700

    move console out to its own module

--- etc/init.js
+++ etc/init.js
@@ -19,26 +19,3 @@ function require (file) {
   throw Error("Cannot to find module '" + file + "'");
 }
 
-var console = {
-  log: function () {
-    var args = Array.prototype.slice.call(arguments, 0);
-    args.unshift(INFO);
-    plv8.elog.apply(undefined, args);
-  },
-  dir: function () {
-    var util = require('util.js');
-    console.log(util);
-    for (var i in util) {
-      console.log(i + " => " + util[i]);
-    }
-    var args = Array.prototype.slice.call(arguments, 0);
-    for (var i = 0; i < args.length; i++) {
-      console.log(util.inspect(args[i], 10));
-    }
-  },
-  error: function () {
-    var args = Array.prototype.slice.call(arguments, 0);
-    args.unshift(ERROR);
-    plv8.elog.apply(undefined, args);
-  }
-};
---
+++ etc/lib/console.js
@@ -0,0 +1,19 @@
+var util = require('util');
+
+exports.log = function() {
+  plv8.elog(INFO, util.format.apply(this, arguments));
+};
+
+exports.info = exports.log;
+
+exports.warn = function () {
+  plv8.elog(WARNING, util.format.apply(this, arguments));
+};
+
+exports.error = function () {
+  plv8.elog(ERROR, util.format.apply(this.arguments));
+};
+
+exports.dir = function (obj) {
+  plv8.elog(INFO, util.inspect(obj));
+};
