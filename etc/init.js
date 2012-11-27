a757b61ed2e4de953f2464f4c8b8c08c66224f50
Audrey Tang <audreyt@audreyt.org>
2012-11-27 21:35:34 +0800

    * Simplify init.ls; no functional changes

--- etc/init.js
+++ etc/init.js
@@ -5,7 +5,7 @@ module = function(pathfilename){
   return this.exports = {};
 };
 require = function(modulename){
-  var filename, delim, c, rp, __dirname, __filename, T, stats, fname, packagejson, pos, suffix, m, body, fn;
+  var filename, delim, c, rp, stats, fname, packageJson, __dirname, __filename, T, pos, m, body, fn;
   filename = modulename;
   delim = require.path_delim;
   c = filename.charAt(0);
@@ -13,69 +13,61 @@ require = function(modulename){
     filename = '/usr/local/plv8/plv8_modules/' + filename;
   }
   rp = require.resolvePath(filename);
-  if (require.loaded[rp] !== undefined) {
+  if (require.loaded[rp] != null) {
     return require.loaded[rp].exports;
-  } else if (require.loading[rp] !== undefined) {
+  }
+  if (require.loading[rp] != null) {
     return require.loading[rp].exports;
   }
-  __dirname = void 8;
-  __filename = void 8;
-  T = void 8;
   stats = native_fs_.statSync(rp);
-  if (stats.isDirectory) {
-    fname = rp + delim + 'package.json';
-    packagejson = new Function('return ' + native_fs_.readFileSync(fname))();
-    if (packagejson.main === undefined) {
-      throw new Error("cannot find module of " + modulename + " (" + fname + " " + packagejson + "})");
+  switch (false) {
+  case !stats.isDirectory:
+    fname = rp + "" + delim + 'package.json';
+    packageJson = new Function("return " + native_fs_.readFileSync(fname))();
+    if (packageJson.main == null) {
+      throw new Error("cannot find module of " + modulename + " (" + fname + " " + packageJson + "})");
     }
     __dirname = rp;
-    __filename = packagejson.main;
-    T = rp + delim + __filename;
+    __filename = packageJson.main;
+    T = rp + "/" + delim + __filename;
     if (!/\.js$/.test(T)) {
       T += '.js';
     }
-  } else {
-    if (stats.isFile) {
-      T = rp;
-    } else {
-      if (native_fs_.existsSync(rp + '.js')) {
-        T = rp + '.js';
-      } else {
-        if (native_fs_.existsSync(rp + '.node')) {
-          T = rp + '.node';
-        } else {
-          if (typeof this['native_' + modulename + '_'] !== 'undefined') {
-            return this['native_' + modulename + '_'];
-          } else {
-            throw new Error('cannot find module of ' + modulename);
-          }
-        }
-      }
-    }
+    break;
+  case !stats.isFile:
+    T = rp;
+    break;
+  case !native_fs_.existsSync(rp + ".js"):
+    T = rp + ".js";
+    break;
+  case !native_fs_.existsSync(rp + ".node"):
+    T = rp + ".node";
+    break;
+  case this["native_" + modulename + "_"] == null:
+    return this["native_" + modulename + "_"];
+  default:
+    throw new Error("cannot find module of " + modulename);
   }
-  pos = void 8;
   pos = T.lastIndexOf(delim);
   __dirname = T.substring(0, pos);
   __filename = T.substring(pos + 1);
-  suffix = __filename.substring(__filename.lastIndexOf('.'));
-  if (suffix === '.js') {
+  switch (__filename.substring(__filename.lastIndexOf('.'))) {
+  case '.js':
     m = new module(T);
     require.loading[rp] = m;
     body = native_fs_.readFileSync(T);
-    fn = new Function("module", "exports", "__dirname", "__filename", body);
+    fn = new Function('module', 'exports', '__dirname', '__filename', body);
     native_fs_.chdir(__dirname);
     m.exports = {};
     fn(m, m.exports, __dirname, __filename);
     require.loaded[rp] = m;
-    require.loading[rp] = undefined;
+    require.loading[rp] = void 8;
     return m.exports;
-  } else {
-    if (suffix === '.node') {
-      m = new module(T);
-      native_fs_.loadso(T, m.exports);
-      require.loaded[T] = m;
-      return m.exports;
-    }
+  case '.node':
+    m = new module(T);
+    native_fs_.loadso(T, m.exports);
+    require.loaded[T] = m;
+    return m.exports;
   }
 };
 require.resolvePath = function(filename){
@@ -83,21 +75,24 @@ require.resolvePath = function(filename){
   path_delim = require.path_delim;
   if (filename[0] === path_delim) {
     return filename;
-  } else {
-    dir = typeof __dirname === 'undefined' ? native_fs_.getcwd() : __dirname;
-    dirar = dir.split(path_delim);
-    for (i$ = 0, len$ = (ref$ = filename.split(path_delim)).length; i$ < len$; ++i$) {
-      t = ref$[i$];
-      if (t === '.') {
-        continue;
-      } else if (t === '..') {
-        dirar.pop();
-      } else {
-        dirar.push(t);
-      }
-    }
-    return dirar.join(path_delim);
   }
+  dir = typeof __dirname != 'undefined' && __dirname !== null
+    ? __dirname
+    : native_fs_.getcwd();
+  dirar = dir.split(path_delim);
+  for (i$ = 0, len$ = (ref$ = filename.split(path_delim)).length; i$ < len$; ++i$) {
+    t = ref$[i$];
+    switch (t) {
+    case '.':
+      continue;
+    case '..':
+      dirar.pop();
+      break;
+    default:
+      dirar.push(t);
+    }
+  }
+  return dirar.join(path_delim);
 };
 require.path_delim = '/';
 require.loading = {};
--- etc/init.ls
+++ etc/init.ls
@@ -9,73 +9,62 @@ require = (modulename) ->
   c = filename.charAt 0
   filename = '/usr/local/plv8/plv8_modules/' + filename if c isnt '.' and c isnt '/' and (filename.indexOf delim) < 0
   rp = require.resolvePath filename
-  if require.loaded[rp] isnt ``undefined`` then return require.loaded[rp].exports else if require.loading[rp] isnt ``undefined`` then return require.loading[rp].exports
-  __dirname = void
-  __filename = void
-  T = void
+  return require.loaded[rp].exports if require.loaded[rp]?
+  return require.loading[rp].exports if require.loading[rp]?
   stats = native_fs_.statSync rp
-  if stats.isDirectory
-    fname = rp + delim + 'package.json'
-    packagejson = (new Function 'return ' +  native_fs_.readFileSync fname)!
-    throw new Error "cannot find module of #modulename (#fname #packagejson})" if packagejson.main is ``undefined``
+  switch
+  | stats.isDirectory
+    fname = "#rp#delim#{\package.json}"
+    package-json = (new Function "return #{ native_fs_.readFileSync fname }")!
+    unless package-json.main?
+        throw new Error "cannot find module of #modulename (#fname #package-json})"
     __dirname = rp
-    __filename = packagejson.main
-    T = rp + delim + __filename
+    __filename = package-json.main
+    T = "#rp/#delim#__filename"
     T += '.js' if T isnt /\.js$/
-  else
-    if stats.isFile
-      T = rp
-    else
-      if native_fs_.existsSync rp + '.js'
-        T = rp + '.js'
-      else
-        if native_fs_.existsSync rp + '.node'
-          T = rp + '.node'
-        else
-          if typeof @['native_' + modulename + '_'] isnt 'undefined' then return @['native_' + modulename + '_'] else throw new Error 'cannot find module of ' + modulename
-  pos = void
+  | stats.isFile
+    T = rp
+  | native_fs_.existsSync "#rp.js"
+    T = "#rp.js"
+  | native_fs_.existsSync "#rp.node"
+    T = "#rp.node"
+  | @["native_#{modulename}_"]?
+      return @["native_#{modulename}_"]
+  | _  => throw new Error "cannot find module of #modulename"
   pos = T.lastIndexOf delim
   __dirname = T.substring 0, pos
   __filename = T.substring pos + 1
-  suffix = __filename.substring __filename.lastIndexOf '.'
-  if suffix is '.js'
+  switch __filename.substring __filename.lastIndexOf '.'
+  | \.js
     m = new module T
     require.loading[rp] = m
     body = native_fs_.readFileSync T
-    fn = new Function "module" "exports" "__dirname" "__filename" body
-    # XXX: shouldn't really chdir, but just calculate logic cwd for require
-    # caller
+    fn = new Function \module \exports \__dirname \__filename body
+    # XXX: shouldn't really chdir, but just calculate logic cwd for require caller
     native_fs_.chdir __dirname
     m.exports = {}
     fn m, m.exports, __dirname, __filename
     require.loaded[rp] = m
-    require.loading[rp] = ``undefined``
+    require.loading[rp] = void
     m.exports
-  else
-    if suffix is '.node'
-      m = new module T
-      native_fs_.loadso T, m.exports
-      require.loaded[T] = m
-      m.exports
+  | \.node
+    m = new module T
+    native_fs_.loadso T, m.exports
+    require.loaded[T] = m
+    m.exports
 
 require.resolvePath = (filename) ->
   path_delim = require.path_delim
-  if filename.0 is path_delim
-    filename
-  else
-    dir = if typeof __dirname is 'undefined' then native_fs_.getcwd! else __dirname
-    dirar = dir.split path_delim
-    for t in filename.split path_delim
-      if t is '.'
-        continue
-      else if t is '..'
-        dirar.pop!
-      else
-        dirar.push t
-    dirar.join path_delim
+  return filename if filename.0 is path_delim
+  dir = __dirname ? native_fs_.getcwd!
+  dirar = dir.split path_delim
+  for t in filename.split path_delim
+    switch t
+    | \.  => continue
+    | \.. => dirar.pop!
+    | _   => dirar.push t
+  dirar.join path_delim
 
 require.path_delim = '/'
-
 require.loading = {}
-
 require.loaded = {}
